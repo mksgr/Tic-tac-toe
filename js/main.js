cf8af34932bedf04208f9f6691918791de79e293
Maksim Grishko <maksimgrishko13@gmail.com>
2022-09-15 00:01:29 +0300

    add switching of crosses and zeroes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const ticItems = document.querySelectorAll(".wrapper__item");
+const userNames = document.querySelectorAll(".header__name");
 
-ticItems.forEach(item => {
+ticItems.forEach((item, i) => {
     item.addEventListener("click", () => {
-        addCrossOrCircle(item, "circle");
+        addCrossOrCircle(item, toggleFigure(i));
+        endGame(i);
     });
 });
+
+
+window.onload = () => {
+    //enterUserName();
+};
 
 function addCrossOrCircle (cell, figure) {
     if (cell.classList.contains("circle") || cell.classList.contains("cross")) {
@@ -14,11 +21,30 @@ function addCrossOrCircle (cell, figure) {
     }
 }
 
-function toggleFigure () {
+function toggleFigure (step) {
     let cross = "cross";
     let circle = "circle";
 
+    if (step % 2 === 0) {
+        step++;
+        return cross;
+    } else {
+        step++;
+        return circle;
+    }
 }
 
-console.log(toggleFigure());
+function enterUserName () {
+    userNames.forEach((name, i) => {
+        console.log(name);
+        name.innerHTML += name.innerHTML = prompt(`Имя ${i + 1} игрока`, '');
+    });
+}
+
+function endGame (num) {
+    if (num === 8) {
+        alert ("Конец!");
+    }
+}
+
 

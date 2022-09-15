5792ae44d81b91f8b78e5d80fef943063433795d
Maksim Grishko <maksimgrishko13@gmail.com>
2022-09-15 14:13:05 +0300

    add popup and fix bug

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-const ticItems = document.querySelectorAll(".wrapper__item");
-const userNames = document.querySelectorAll(".header__name");
+const ticItems = document.querySelectorAll(".wrapper__item"),
+      userNames = document.querySelectorAll(".header__name"),
+      popup = document.querySelector(".popup"),
+      welcomeBtn = document.querySelector(".popup__btn");
+
+const animationTiming = {
+    duration: 300,
+    fill: "forwards",
+};
+
+
+let counter = 0;
+let allWinCombination = ["012", "345", "678", "036", "147", "258", "048", "246"];
+let crossStr = "";
+let circleStr = "";
 
 ticItems.forEach((item, i) => {
     item.addEventListener("click", () => {
-        addCrossOrCircle(item, toggleFigure(i));
-        endGame(i);
+        counter += 1;
+        addCrossOrCircle(item, toggleFigure(counter));
+        endGame();
+        winUser(item, i);
     });
 });
 
@@ -26,11 +41,9 @@ function toggleFigure (step) {
     let circle = "circle";
 
     if (step % 2 === 0) {
-        step++;
-        return cross;
-    } else {
-        step++;
         return circle;
+    } else {
+        return cross;
     }
 }
 
@@ -41,10 +54,43 @@ function enterUserName () {
     });
 }
 
-function endGame (num) {
-    if (num === 8) {
-        alert ("Конец!");
+function endGame () {
+    if (counter === 9) {
+        alert ("Конец игры!");
     }
 }
+
+function winUser (cell, num ) {
+    if (cell.classList.contains("cross")) {
+        crossStr += num;
+    } else {
+        circleStr += num;
+    }
+    console.log(crossStr);
+    console.log(circleStr);
+
+    whoWin(crossStr, allWinCombination);
+    whoWin(circleStr, allWinCombination);
+}
+
+function whoWin (str, arr) {
+    arr.forEach(item => {
+        if (str.includes(item)) {
+            alert(`cross win!`);
+        }
+    });
+}
+
+
+welcomeBtn.addEventListener("click", () => {
+    popup.animate(
+        [
+            {opacity: '100%'},
+            {opacity: '50%'},
+            {opacity: '0%', display: 'none'}
+        ], animationTiming
+    );
+    popup.classList.add("close");
+});
 
 
